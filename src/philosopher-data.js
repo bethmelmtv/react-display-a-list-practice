6ec770b3ede81035781b317723e7f207f0e20ac1
Beth <bethmelmtv@gmail.com>
2022-04-28 14:47:40 -0700

    finished fourth function

---
+++ src/philosopher-data.js
@@ -0,0 +1,29 @@
+
+export const philosophers = [
+  {
+    name: 'aristotle',
+    country: 'greece',
+    work: [
+      'On the Soul',
+      'Golden mean',
+      'Hexis',
+    ],
+  },
+  {
+    name: 'plato',
+    country: 'greece',
+    work: [
+      'theory of forms',
+      'republic',
+    ],
+  },
+
+  {
+    name: 'immanuel kant',
+    country: 'Königsberg',
+    work: [
+      'transcendental idealism',
+      'transcendental deduction',
+    ],
+  },
+];
